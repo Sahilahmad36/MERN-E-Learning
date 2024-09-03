02b8852b0b04aeb2041230429119fe4c7f47e164
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2024-09-04 01:16:31 +0530

    Update Courses.jsx

--- frontend/src/pages/courses/Courses.jsx
+++ frontend/src/pages/courses/Courses.jsx
@@ -1,23 +1,69 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+// import React from "react";
+// import "./Courses.css";
+// import { CourseData } from '../../context/CourseContext';
+// import CourseCard from "../../components/coursecard/CourseCard";
+
+// const Courses = () => {
+
+// const {courses} = CourseData();
+// console.log(courses);
+//   return (
+//     <div className="courses">
+//     <h2>Available Courses</h2>
+//     <div className="course-container">
+//       {courses && courses.length>0 ? (
+//         courses.map((e)=> <CourseCard key={e._id} course={e}/> )
+//         ) : (
+//           <p>No Courses Yet!</p>
+//         )}
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default Courses;
+import React, { useState } from "react";
 import "./Courses.css";
-import { CourseData } from '../../context/CourseContext';
+import { CourseData } from "../../context/CourseContext";
 import CourseCard from "../../components/coursecard/CourseCard";
 
 const Courses = () => {
+  const { courses } = CourseData();
+  const [searchQuery, setSearchQuery] = useState("");
 
-const {courses} = CourseData();
-console.log(courses);
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredCourses = courses.filter((course) =>
+    course.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="courses">
-    <h2>Available Courses</h2>
-    <div className="course-container">
-      {courses && courses.length>0 ? (
-        courses.map((e)=> <CourseCard key={e._id} course={e}/> )
+     <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by course title..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="course-search"
+        />
+      </div>
+      <h2>Available Courses</h2>
+
+     
+
+      <div className="course-container">
+        {filteredCourses && filteredCourses.length > 0 ? (
+          filteredCourses.map((course) => (
+            <CourseCard key={course._id} course={course} />
+          ))
         ) : (
-          <p>No Courses Yet!</p>
+          <p>No Courses Found!</p>
         )}
-    </div>
+      </div>
     </div>
   );
 };
