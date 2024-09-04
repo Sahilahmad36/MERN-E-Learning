5a9c11fbee417d2965b3f394efeced3d8346a02c
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2024-09-04 13:08:59 +0530

    Update Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./Footer.css";
+import {Link} from "react-router-dom";
 import { FaInstagram, FaYoutube, FaLinkedin } from "react-icons/fa"; // Import icons
 
 
