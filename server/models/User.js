b1ef9fe43ac5585257457013b0424a2d70787e06
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2024-09-05 13:05:06 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,10 @@ const schema = new mongoose.Schema({
         type: String,
         required: true,
     },
+     number: {
+        type: Number,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
